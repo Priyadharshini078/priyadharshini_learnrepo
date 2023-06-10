a6863a9c0e0454f1cde5db3aa85adf8e494f1abc
Priyadharshini S <prithipriya1980@gmail.com>
2023-06-10 13:19:34 +0530

    2nd.py

--- assignment 2/2nd.py
+++ assignment 2/2nd.py
@@ -1,3 +1,9 @@
+#For this assignment, let’s use example 5. Store the example 5 json in a file ex5.json.
+#Read the json using python, store in a dictionary named ex5
+#Write a code to add a batter named Coffee for “donut” with name “Old Fashoined”.
+#Replace the ex5.json with the new data.
+
+
 import json
 
 with open('ex5.json') as json_file:
